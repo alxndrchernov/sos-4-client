2f29614b9fb2551dfcfc0359b8754877488c286d
alxndrchernov <incoswer@gmail.com>
2022-04-22 16:01:54 +0300

    Initial Commit

---
+++ SOS4/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  SOS4
+//
+//  Created by Alexander Chernov on 22.04.2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
